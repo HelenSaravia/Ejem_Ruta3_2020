8f35f7c6990bf38f6f10eafb10b1f7ca03447923
HelenSaravia <66745544+HelenSaravia@users.noreply.github.com>
2020-06-18 08:50:48 -0600

    Add files via upload

---
+++ holamundo.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 15 14:16:16 2020
+
+@author: Docente Superate
+"""
+print("hola mundo")
+
+print('hola mundo')
+
+print("hola", "adios")
+
+print("hola")
+print("adios")
+
+print("Hola", end="")
+print("Adiós")
+
+texto = " y "
+print("Hola", end=f"{texto}")
+print("Adiós")
+
